a7df029d3baf3c17130381a1e9f97b731c10fd42
Svyatoslav <54244300+svatos331@users.noreply.github.com>
2023-01-16 18:22:44 +0300

    init project

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+
+
+
+
+
+
+root.render(
+  <React.StrictMode>
+    <div>123</div>
+  </React.StrictMode>
+);
+
