95cab249ce393aa44148e956ea220205b1dbafd3
Svyatoslav <54244300+svatos331@users.noreply.github.com>
2023-01-18 20:03:11 +0300

    [PD-8] Created component Insights

--- src/Colors/index.ts
+++ src/Colors/index.ts
@@ -1,13 +1,15 @@
 enum Colors {
   WHITE = '#FFFFFF',
   BLACK1 = '#333333',
-  BLUE = '#2E6CEE;',
+  BLUE = '#2E6CEE',
   SKYBLUE = '#D5E2FC',
   BLACK2 = '#11142D',
   GREY1 = '#808191',
   GREY2 = '#969DB1',
   GREY3 = '#E1E3E6',
   WHITE1 = '#EFF5FF',
+  BLUE1 = '#4E87FF',
+  RGBA_LIGHTBLUE = 'rgba(255, 255, 255, 0.3)',
 }
 
 export default Colors;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './Router';
-import './18n.ts';
+import Router from 'Router';
+import '18n.ts';
 import AddBlock from 'Components/UI-KIT/AddBlock';
 import CourseCard from 'Components/UI-KIT/CourseCard';
-import CardNews from './Components/UI-KIT/CardNews';
-import './index.css';
+import CardNews from 'Components/UI-KIT/CardNews';
+import Insights from 'Components/UI-KIT/Insights';
+import 'index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,6 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Suspense fallback={<div>...loading</div>}>
+      <Insights />
       <CardNews />
       <CourseCard />
       <AddBlock />
