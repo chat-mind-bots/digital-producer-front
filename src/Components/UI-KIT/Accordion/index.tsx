2a539d1bf81f6fa87d189d14fa3672f6a6b45828
Svyatoslav <54244300+svatos331@users.noreply.github.com>
2023-01-20 18:29:05 +0300

    [PD-19] Created component Accordion and logic component. Fix console errors.

---
+++ src/Components/UI-KIT/Accordion/index.tsx
@@ -0,0 +1,63 @@
+import { FC, useState } from 'react';
+import { ReactComponent as ArrowDown } from 'Icons/ArrowDown.svg';
+import * as ST from './styled';
+
+type AccordionProps = {
+  array: AccordionType[];
+  handleClick: (props: HandleClickType) => void;
+};
+
+export type AccordionType = {
+  id: number;
+  name: string;
+  items: {
+    id: number;
+    name: string;
+    isActive: boolean;
+  }[];
+};
+
+export type HandleClickType = {
+  moduleId: number;
+  itemId: number;
+};
+
+const Accordion: FC<AccordionProps> = ({ array, handleClick }) => {
+  const [open, setOpen] = useState<number>();
+
+  return (
+    <ST.Accordion>
+      <ST.Title>Модули курса:</ST.Title>
+      <ST.Wrapper>
+        {array.map((module, indexModule) => (
+          <ST.WrapperModule key={`Accordion-module-${module.id}`}>
+            <ST.Name isActive={open === module.id}>
+              модуль {indexModule + 1}: {module.name}
+              <ArrowDown
+                onClick={() =>
+                  setOpen(open === module.id ? undefined : module.id)
+                }
+              />
+            </ST.Name>
+            <ST.WrapperItems isActive={open === module.id}>
+              {module.items.map((item, indexItem) => (
+                <ST.Item
+                  onClick={() =>
+                    handleClick({ moduleId: module.id, itemId: item.id })
+                  }
+                  isActive={item.isActive}
+                  key={`Accordion-item-${item.id}`}
+                >
+                  <ST.Number>{indexItem + 1}.</ST.Number>
+                  {item.name}
+                </ST.Item>
+              ))}
+            </ST.WrapperItems>
+          </ST.WrapperModule>
+        ))}
+      </ST.Wrapper>
+    </ST.Accordion>
+  );
+};
+
+export default Accordion;
