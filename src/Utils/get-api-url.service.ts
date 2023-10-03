3fa02d80e731e77e35c2c31a1642342f580777e5
Petr Molchanov <p.molchanov@rogii.com>
2023-10-03 00:18:20 -0300

    docker update

--- src/Utils/get-api-url.service.ts
+++ src/Utils/get-api-url.service.ts
@@ -1,6 +1,6 @@
 import * as process from "process";
 
 export const getApiUrlService = () =>
-	`${process.env.REACT_APP_MODE === "LOCAL" ? "http" : "https"}://${
+	`${process.env.REACT_APP_MODE === "LOCAL" ? "http" : "https"}://back.${
 		process.env.REACT_APP_API_URL
 	}/api`;
